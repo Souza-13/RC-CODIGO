6f15ee6e1e642baa3c0c3857958b20fa4f691478
Lucas Souza <ldesouzasa@gmail.com>
2024-05-10 11:36:31 -0300

    RemedyCheck

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var btnSignin = document.querySelector("#signin");
+    var btnSignup = document.querySelector("#signup");
+    var body = document.querySelector("body");
+
+    btnSignin.addEventListener("click", function () {
+       body.className = "sign-in-js"; 
+    });
+
+    btnSignup.addEventListener("click", function () {
+        body.className = "sign-up-js";
+    });
+});
